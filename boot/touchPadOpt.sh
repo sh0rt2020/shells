513f0f1218dbfdcee091411304495a8d4e326e03
well <waynesun1990@gmail.com>
2020-11-01 13:37:20 +0800

    Optimize touch pad.

---
+++ boot/touchPadOpt.sh
@@ -0,0 +1,8 @@
+#设置轻触触摸板
+defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+#三指拖拽
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
