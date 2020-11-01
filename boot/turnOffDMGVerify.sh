3ddde59751d2be2e0387c16b1597275c797f90f5
well <waynesun1990@gmail.com>
2020-11-01 13:56:58 +0800

    Turn off DMG file verify.

---
+++ boot/turnOffDMGVerify.sh
@@ -0,0 +1,4 @@
+#关闭镜像文件验证
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
