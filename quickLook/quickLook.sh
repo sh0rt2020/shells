06aef565f3509858ee478339410410047be285b5
well <waynesun1990@gmail.com>
2020-11-01 17:33:16 +0800

    Add different quick look plugins.

---
+++ quickLook/quickLook.sh
@@ -0,0 +1,4 @@
+#各式各样的 quick look 插件
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook qlvideo provisionql quicklookapk
+#https://github.com/sindresorhus/quick-look-plugins
+#brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize suspicious-package quicklookase qlvideo
