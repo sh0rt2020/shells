3f819d572195dd4093d12464d10b392d50fdc239
well <waynesun1990@gmail.com>
2020-11-17 21:36:31 +0800

    Implement CustomDock with Swift.

--- dock/CustomDock.swift
+++ dock/CustomDock.swift
@@ -1,2 +1,24 @@
 import Foundation
 
+@UIApplicationMain
+
+class CustomDock {
+    
+    static func shouldRemoveIconWithFile(file: String) -> Bool {
+        static let appsToRemoveIcon = ["Siri", "Contacts", "Notes", "Reminders", "Maps", "Photos", "Messages", "FaceTime", "Pages", "Numbers", "Keynote", "iTunes", "iBooks", "App%20Store"]
+        for let icon in appsToRemoveIcon {
+            var fullIconPath = "file:///Applications/\(icon).app/"
+            if fullIconPath == file {
+                return true
+            }
+        }
+        return false
+    }
+
+    static func customDockIcons() {
+        let plistPath = NSHomeDirectory() + "Library//Preferences/com.apple.dock.plist"
+        var plistData = Dictionary()
+        
+    }
+}
+
---
+++ dock/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  
+//
+//  Created by sun well on 2020/11/17.
+//
+
+import Foundation
